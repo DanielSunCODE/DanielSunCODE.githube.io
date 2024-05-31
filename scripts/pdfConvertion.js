0767aa0ab7c73e9a3cac0809fc4442561d119ca5
DANIEL_CODE <82288076+DanielSunCODE@users.noreply.github.com>
2024-05-31 08:59:11 -0600

    Initial commit

---
+++ scripts/pdfConvertion.js
@@ -0,0 +1,38 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', ()=>{
+    const $saveBtn = document.querySelector('.save-btn');
+    let pixels = drawingSection.children.length;
+    $saveBtn.addEventListener('click', ()=>{
+        colorsSidebar.style.display = 'none';
+        pixelsSidebar.style.display = 'none';
+        eraserBtn.style.display = 'none';
+        const $elementoParaConvertir = document.body;
+        html2pdf()
+        .set({
+            margin: 1.6,
+            filename: 'mydraw.pdf',
+            image: {
+                type: 'jpeg',
+                quality: 0.98,
+            },
+            html2canvas: {
+                scale: 3,
+                letterRendering: true,
+            },
+            jsPDF: {
+                unit: 'in',
+                format: 'a3',
+                orientation: 'portrait',
+            }
+        })
+        .from($elementoParaConvertir)
+        .save()
+        .catch(err => console.log(err));
+        setTimeout(()=>{
+            colorsSidebar.style.display = 'block';
+            pixelsSidebar.style.display = 'block';
+            eraserBtn.style.display = 'flex';
+        }, 150)
+        alert('Ha sido guardado');
+    });
+});
